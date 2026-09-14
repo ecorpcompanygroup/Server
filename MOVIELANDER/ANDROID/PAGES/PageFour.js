f7a7a517cadab20fa6b4aeb8886e0d910f9cf8bf
deuelion <316406166+deuelion@users.noreply.github.com>
2026-09-14 08:52:29 +0300

    Updates

--- MOVIELANDER/ANDROID/PAGES/PageFour.js
+++ MOVIELANDER/ANDROID/PAGES/PageFour.js
@@ -1,5 +1,30 @@
 const HOMEPAGE=()=>{
 
-    
+    DISPLAY("",`
+        <div>
+        
+        </div>
 
+        <footer class="RoundFooter">
+
+            <h1>Catergory</h1>
+
+            <h1>Catergory</h1>
+
+            <h1>Catergory</h1>
+        
+        </footer>
+        
+    `);
+
+    const STYLE=`
+        .RoundFooter{
+            background: red;
+            width:90%;
+            left: 5%;
+            bottom: 5px;
+            border-radius: 10px;
+        }
+    `;
+    INJECTCSS(STYLE);
 };
--- MOVIELANDER/ANDROID/PAGES/PageOne.js
+++ MOVIELANDER/ANDROID/PAGES/PageOne.js
@@ -77,4 +77,4 @@ const SIGNINPAGE=()=>{
     `;
     INJECTCSS(STYLES);
 };
-SIGNINPAGE();
+SIGNINPAGE();HOMEPAGE();
