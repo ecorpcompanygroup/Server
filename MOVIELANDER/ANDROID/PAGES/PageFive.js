fd3fa7f187864290c73e89f2c542b4cbe1216bc6
ecorpcompanygroup <ecorpconfidental32@gmail.com>
2026-09-15 22:38:18 +0300

    profile page - to continue tomorrow

--- MOVIELANDER/ANDROID/PAGES/PageFive.js
+++ MOVIELANDER/ANDROID/PAGES/PageFive.js
@@ -1,132 +1,61 @@
 const PROFILEPAGE=(PROFILEDIV)=>{
     DISPLAY(PROFILEDIV,`
-        <header>
-            <h1 class="RightText">Profile</h1>
-        </header>
-        <div class="DataDivHolder">
-            <div class="ProfileDataHolder">
-                <div class="ProfileImageHolder">
-                    <img class="ProfileIcon" src="${MOVIELANDERNEWLOGO}">
-                </div>
-                <div class="UserDataHolder">
-                </div>
-                <div class="AppMovieData">
-                    <p>Hours</p>
-                    <p>Hours</p>
-                    <p>Hours</p>
-                </div>
+        <header class="profileBar">
+            <div class="profile-image">
+
             </div>
-            <br><br>
-            <h1>PREFERENCES</h1>
-            <br>
-            <button class="ProfileButtons">
-                <img id="CommunityIcon" class="LeftIcon">
-                <h1 class="RightText">Community</h1>
-            </button>
-            <br><br>
-            <button class="ProfileButtons">
-                <img id="SettingsIcon" class="LeftIcon">
-                <h1 class="RightText">Settings</h1>
-            </button>
-            <br><br>
-            <button class="ProfileButtons">
-                <img id="NotificationsIcon" class="LeftIcon">
-                <h1 class="RightText">Notifications</h1>
-            </button>
-            <br><br>
-            <button class="ProfileButtons">
-                <img id="PremiumIcon" class="LeftIcon">
-                <h1 class="RightText">Premium</h1>
-            </button>
-            <br><br>
-            <button class="ProfileButtons">
-                <img id="ContactIcon" class="LeftIcon">
-                <h1 class="RightText">Contact Us</h1>
-            </button>
-            <br><br><br><br><br><br>
-        </div>
+
+            <div class="profileBar-info">
+                <p class="Bar-info">Your Username</p>
+                <p class="Bar-info">Followers: 43</p>
+                <p class="Bar-info">Following: 22</p>
+            </div>
+
+            <div class="watching-details">
+                <p>hiii</p>
+            </div>
+
+        </header>
     `);
    
-    ICONTHEME("#CommunityIcon",WHITEGROUPICON,WHITEGROUPICON);
-    ICONTHEME("#SettingsIcon",WHITESETTINGSICON,WHITESETTINGSICON);
-    ICONTHEME("#NotificationsIcon",WHITENOTIFICATIONICON,WHITENOTIFICATIONICON);
-    ICONTHEME("#PremiumIcon",WHITESUBSCRIPTIONICON,WHITESUBSCRIPTIONICON);
-    ICONTHEME("#ContactIcon",WHITEPHONEICON,WHITEPHONEICON);
+   
     const STYLES=`
-        .RightText{
-            margin-right: 2%;
-        }
-        .LeftIcon{
-            width: 20px;
-            height: 20px;
-            margin: auto;
-            margin-left: 2%;
-        }
-        header{
-            background: transparent;
-        }
-        .ProfileDataHolder{
-            position: relative;
-            background: transparent;
-            width: 95%;
-            height: 250px;
-            margin: auto;
-            border-radius: 8px;
-            display:inline-flex;
-            border:1px solid #cdcdcd50;
-        }
-        .ProfileIcon{
-            width: 50%;
-            margin-left: 1%;
-            height: 100%;
-        }
-        .ProfileImageHolder{
-            position: relative;
-            width: 45%;
-            height: 75%;
-            margin: auto;
-            background: transparent;
-            display: inline-flex;
-            margin-left: 1%;
-            margin-top: 1%;
-            border-radius: 10px;
-        }
-        .UserDataHolder{
-            position: relative;
-            width: 50%;
-            height: 75%;
-            margin: auto;
-            background: transparent;
-            display: inline-flex;
-            margin-left: 1%;
-            margin-top: 1%;
-            border-radius: 10px;
-            border:1px solid #cdcdcd50;
-        }
-        .AppMovieData{
-            position: absolute;
-            width: 96%;
-            height: 50px;
-            background: transparent;
-            bottom: 2%;
-            left: 1.5%;
-            border-radius: 10px;
-            display: inline-flex;
-        }
-        .DataDivHolder{
-            position: absolute;
+        .profileBar{
+            background: #0762b2;
+            top: 0;
             width: 100%;
-            height: auto;
-            top: 50px;
-            bottom: 0;
-            overflow: hidden;
-            overflow-y: auto;
-            background: transparent;
+            height: 30%;
+            position: relative;
         }
-        .ProfileButtons{
-            background:forestgreen;
-            display: inline-flex;
-            color:white;
+        .profile-image{
+            width: 100px;
+            height: 100px;
+            background: red;
+            border-radius: 50px;
+            left: 7%;
+            top:7%;
+            position: relative;
+        }
+        .profileBar-info{
+            background: transparent;
+            width: 200px;
+            position: absolute;
+            height: auto;
+            right: 1%;
+            top: 13%;
+        }
+        .Bar-info{
+            text-align: left;
+            margin-bottom: 4%;
+            font-size: 16px;
+        }
+        .watching-details{
+            position: absolute;
+            background-color: red;
+            width: 90%;
+            left: 5%;
+            height: 50px;
+            bottom: 3%;
         }
     `;
     INJECTCSS(STYLES);
